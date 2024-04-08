e60285d7824203d2634669a028bf874eff9bfb63
MykolaMatsko <mykola.matsko@gmail.com>
2024-04-08 22:51:35 +0300

    some changes

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -15,12 +15,7 @@ namespace Mango.Services.ProductAPI
 			// Add services to the container.
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-			builder.Services.
 
-			IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
-			builder.Services.AddSingleton(mapper);
-			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-			builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
